0d4abfe21caf3ef010be9e9f97172b0a7de98dc2
vim <vim.background@gmail.com>
2026-09-16 22:14:31 +0800

    ci: enhance GitHub Actions workflow with setup-android, pwsh defaults, and lint suppression

--- modules/android/build.gradle.kts
+++ modules/android/build.gradle.kts
@@ -54,6 +54,10 @@ android {
     buildFeatures {
         compose = true
     }
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
+    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
